f1bdcc67bde62ffd8a1996d7906db740629ead9a
Raiyan-R11 <raiyanriz11@gmail.com>
2026-09-19 14:45:23 +0400

    docker - challenge , scaling and load balancing

--- 5-docker/containers-intro/challenge/counter-app.py
+++ 5-docker/containers-intro/challenge/counter-app.py
@@ -1,19 +1,22 @@
 from flask import Flask
 import redis
+import socket
 
 app = Flask(__name__)
 
 redis_client = redis.StrictRedis(host='mydb_redis', port=6379, db=0)
 
+container_id = socket.gethostname()
+
 @app.route('/')
 def welcome():
-    return 'Welcome to the Counter App!'
+    return f'Welcome to the Counter App! (Container: {container_id})'
 
 @app.route('/counter')
 def counter():
     # Increment the counter in Redis
     count = redis_client.incr('counter')
-    return f'Counter: {count}'
+    return f'Visit Counter: {count} , (Container: {container_id})'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
